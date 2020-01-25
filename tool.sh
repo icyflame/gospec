224168edd893afcbd101881c5ecefd8509f9fb31
Siddharth Kannan <mail@siddharthkannan.in>
2020-01-26 00:21:55 +0900

    Display test output appropriately

--- tool.sh
+++ tool.sh
@@ -21,14 +21,30 @@ colorize () {
 count_action () {
     local output="${1}"
     local action="${2}"
-    echo $output | jq -r '[.[] | select(.Action == "'"$action"'")] | length'
+    get_by_action "$output" "$action" | jq -r '. | length'
 }
 
-get_tests_by_action () {
+get_by_action () {
     local output="${1}"
     local action="${2}"
-    echo $output | jq -r '[.[] | select(.Action == "'"$action"'")]'
+    echo "$output" | jq -r '[.[] | select(.Action == "'"$action"'")]'
 }
+
+get_tests () {
+    local output="${1}"
+    echo "$output" | jq -c '[.[] | select(.Test != null)]'
+}
+
+get_by_test_and_action () {
+    local output="${1}"
+    local test_name="${2}"
+    local action="${3}"
+    echo "$output" | \
+        jq '[.[] | select(.Test == "'"$test_name"'") | select(.Action == "'"$action"'")]'
+}
+
+
+# --- Printing various parts of the output
 
 test_summary () {
     local output="${1}"
@@ -48,15 +64,8 @@ test_summary () {
     echo $result
 }
 
-output=$($cmd_go_test | $cmd_jq)
-
-get_tests () {
-    local output="${1}"
-    echo $output | jq -c '[.[] | select(.Test != null)]'
-}
-
 gospec () {
-    cmd_go_test="go test -json ./..."
+    cmd_go_test="go test -count=1 -v -json ./..."
     cmd_jq="jq -c --slurp ."
 
     output=$($cmd_go_test | $cmd_jq)
@@ -71,7 +80,7 @@ gospec () {
         println
         println "Failing tests:"
 
-        get_tests_by_action "$output_tests" "fail" | \
+        get_by_action "$output_tests" "fail" | \
             jq -c 'group_by(.Package) | .[]' | \
             while read failed_package; do
                 package_name=$(echo "$failed_package" | jq -r '[.[].Package] | unique | .[0]')
@@ -81,6 +90,9 @@ gospec () {
                     jq -r 'sort_by(.Test) | reverse | .[].Test' | \
                     while read test_name; do
                         println "\t$test_name";
+                        local action="output";
+                        get_by_test_and_action "$output_tests" "$test_name" "output" | \
+                            jq -r '.[].Output | select(test("(FAIL|RUN)") | not) | sub("\\n"; "")'
                     done
                 done
     fi
